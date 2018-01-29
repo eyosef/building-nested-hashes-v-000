18ab8cb05832ec73f54efeeba409ddd4d0d3f14d
Eden Mesfun <emesfun@gmail.com>
2018-01-29 15:47:02 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         :benvolio => {:name =>, :age =>, :attitude =>}
-        :mercuto => {}
+        :mercuto => {:name =>, :age =>, :attitude =>}
       ]
    },
    :capulet => {
