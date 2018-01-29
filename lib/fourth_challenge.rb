c722338c1336b2e308d702ac5b77e58b521de0d5
Eden Mesfun <emesfun@gmail.com>
2018-01-29 16:00:07 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -19,8 +19,9 @@ def fourth_challenge
       ]
    }
 }
-epic_tragedy[:montague][:hero][:status] = "dead",
-epic_tragedy[:capulet][:heroine][:status] = "dead",
+
 end
 
+epic_tragedy[:montague][:hero][:status] = "dead",
+epic_tragedy[:capulet][:heroine][:status] = "dead",
 puts epic_tragedy
