a2b2dbdea6d4684d373ab75648404b0ee531e2ef
Eden Mesfun <emesfun@gmail.com>
2018-01-29 15:49:38 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,9 +10,9 @@ def fourth_challenge
       ]
    },
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :patriarch => {:name "Lord Capulet", :age "50"},
+      :matriarch => {:name "Lady Capulet", :age "51"},
+      :heroine => {:name "Juliet", :age "15", :status "alive"},
       :heroine_friends => [
         :steven => {}
         :nurse => {}
